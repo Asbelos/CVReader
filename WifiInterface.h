2f0818f3b6e5377807a7d501eb27450461731db0
SteveT <mstevetodd@mstevetodd.com>
2020-08-20 11:28:17 -0400

    replaced with Chris' latest to (hopefully) fix conflicts

--- WifiInterface.h
+++ WifiInterface.h
@@ -24,6 +24,8 @@
 #include <Arduino.h>
 #include <avr/pgmspace.h>
 
+typedef void (*HTTP_CALLBACK)(Print * stream, byte * cmd);
+
 class WifiInterface {
 
  public:
@@ -31,15 +33,16 @@ class WifiInterface {
           const __FlashStringHelper* hostname, const __FlashStringHelper* servername, int port);
     static void loop();
     static void ATCommand(const byte * command);
-
+    static void setHTTPCallback(HTTP_CALLBACK callback);
     
   private:
     static Stream * wifiStream;
     static DCCEXParser parser;
     static bool setup2( const __FlashStringHelper* SSSid, const __FlashStringHelper* password,
            const __FlashStringHelper* hostname, const __FlashStringHelper* servername, int port);
-    static bool checkForOK(const unsigned int timeout, const char* waitfor, bool echo);
-    static bool isHTML();
+    static bool checkForOK(const unsigned int timeout, const char* waitfor, bool echo, bool escapeEcho=true);
+    static bool isHTTP();
+    static HTTP_CALLBACK httpCallback;
     static bool connected;
     static bool closeAfter;
     static byte loopstate;
